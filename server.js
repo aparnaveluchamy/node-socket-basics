dbd9762bd79fd92f0fb541bfb807ebe111bb5512
Aparna <aparnaveluchamy@gmail.com>
2016-03-30 16:47:09 -0400

    Basic Server

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-var PORT = 
+var PORT = process.env.PORT || 3000;
+var express = require('express');
+
+var app = express();
+var http = require('http').Server(app);
+
+app.use(express.static(__dirname + '/public'));
+
+http.listen(PORT, function() {
+	console.log('Server started');
+});
